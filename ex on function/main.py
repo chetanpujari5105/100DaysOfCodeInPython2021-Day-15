e311a8a7c503f7a2365492c3faaa216134f59f85
Chetan Pujari <57452831+chetanpujari5105@users.noreply.github.com>
2021-01-18 23:08:06 +0530

    Add files via upload

---
+++ ex on function/main.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+#by declaring variable
+def greet_with(name,location):
+    print(f"hello {name} good morning ")
+    print(f"right you are at {location} location ")
+greet_with("chetan","198.256.254.231.21")
+greet_with(location = "198.256.244.789.54.12",name = "sapna")
